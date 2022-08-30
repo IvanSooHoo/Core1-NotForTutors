53109f3fe44e5d2764b17770187b3d3fc028f862
Ivan <103798094@student.swin.edu.au>
2022-08-30 19:42:00 +1000

    Added sound for clicks and logs to keep track and added another language which is Malay

--- app/src/main/java/com/example/core1_ivansoohoo/MainActivity.kt
+++ app/src/main/java/com/example/core1_ivansoohoo/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     var _score: Int = 0
     lateinit var mediaPlayer: MediaPlayer
+    lateinit var clicks: MediaPlayer
 
     // no need to call prepare(); create() does that for you
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,52 +24,74 @@ class MainActivity : AppCompatActivity() {
         val steal = findViewById<Button>(R.id.Steal)
         val Reset = findViewById<Button>(R.id.Reset)
         val result = findViewById<TextView>(R.id.Result)
-        mediaPlayer = MediaPlayer.create(applicationContext,R.raw.battle_crowd_celebration)
-       // mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-        Log.i("LIFECYCLE","MediaPlayer Successfully Created $mediaPlayer")
+        mediaPlayer = MediaPlayer.create(applicationContext, R.raw.battle_crowd_celebration)
+        clicks = MediaPlayer.create(applicationContext, R.raw.click)
+        // mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        Log.i(
+            "LIFECYCLE",
+            "MediaPlayer Successfully Created $mediaPlayer"
+        )//check for successful creation
+        Log.i(
+            "LIFECYCLE",
+            "MediaPlayer Successfully Created $clicks"
+        )//check for successful creation
+
         savedInstanceState?.let {
             _score = it.getInt("SCORE")
             result.text = _score.toString()
         }
 
-        score.setOnClickListener(){
-
-            if(_score in 0 .. 14){
+        score.setOnClickListener() {
+            clicks.start()
+            if (_score in 0..14) {
                 _score = Add()
-                Log.i("LIFECYCLE","MediaPlayer is still $mediaPlayer")
 
-                if(_score in 0..4){
+                Log.i(
+                    "LIFECYCLE",
+                    "is CLICKS on score button playing? ${clicks.isPlaying}"
+                )//checks to see if sound is playing upon score click
+                Log.i(
+                    "LIFECYCLE",
+                    "MediaPlayer is still $mediaPlayer"
+                )//this is to make sure that everytime click it will point to the same media player
+
+
+                if (_score in 0..4) {
                     result.setTextColor(Color.BLACK)
                 }
-                if(_score in 5..9){
+                if (_score in 5..9) {
                     result.setTextColor(Color.GREEN)
                 }
-                if(_score in 10..15){
+                if (_score in 10..15) {
                     result.setTextColor(Color.BLUE)
                 }
-                if(_score == 15){
+                if (_score == 15) {
                     mediaPlayer.seekTo(0)
                     mediaPlayer.setVolume(10F, 20F)
                     mediaPlayer.start()
-                    Log.i("LIFECYCLE"," IS IT PLAYING? ${mediaPlayer.isPlaying}")
-                    Log.i("LIFECYCLE","The media has played successfully")
+                    Log.i("LIFECYCLE", " IS IT PLAYING? ${mediaPlayer.isPlaying}")
+                    Log.i("LIFECYCLE", "The media has played successfully")
                 }
 
             }
             result.text = _score.toString()
 
         }
-        steal.setOnClickListener(){
-
-            if(_score in 1 .. 15){
+        steal.setOnClickListener() {
+            clicks.start()
+            Log.i(
+                "LIFECYCLE",
+                "is CLICKS on steal button playing? ${clicks.isPlaying}"
+            )//checks to see if CLICKS is playing upon button click
+            if (_score in 1..15) {
                 _score = Steal()
-                if(_score in 0..4){
+                if (_score in 0..4) {
                     result.setTextColor(Color.BLACK)
                 }
-                if(_score in 5..9){
+                if (_score in 5..9) {
                     result.setTextColor(Color.GREEN)
                 }
-                if(_score in 10..15){
+                if (_score in 10..15) {
                     result.setTextColor(Color.BLUE)
                 }
 
@@ -79,17 +102,25 @@ class MainActivity : AppCompatActivity() {
         Reset.setOnClickListener()
         {
             _score = reset()
-            mediaPlayer.stop()
-            mediaPlayer.prepare()
-            Log.i("LIFECYCLE","IS MEDIAPLAYER PLAYING? : ${mediaPlayer.isPlaying}")
+            mediaPlayer.stop()//stops the media player
+            mediaPlayer.prepare()//prepares media player for next play.
+            clicks.start()
+            Log.i(
+                "LIFECYCLE",
+                "is CLICKS on reset button playing? ${clicks.isPlaying}"
+            )//test whether sound is playing upon reset click
+            Log.i(
+                "LIFECYCLE",
+                "IS MEDIAPLAYER PLAYING? : ${mediaPlayer.isPlaying}"
+            )//test to see if MEDIAplayer is still playing
 
-            if(_score in 0..4){
+            if (_score in 0..4) {
                 result.setTextColor(Color.BLACK)
             }
-            if(_score in 5..9){
+            if (_score in 5..9) {
                 result.setTextColor(Color.GREEN)
             }
-            if(_score in 10..15){
+            if (_score in 10..15) {
                 result.setTextColor(Color.BLUE)
             }
             result.text = _score.toString()
@@ -99,23 +130,25 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
     private fun Add() = _score + 1
     private fun Steal() = _score - 1
     private fun reset() = _score - _score
 
     override fun onDestroy() {
         super.onDestroy()
-        if(mediaPlayer != null){
+        if (mediaPlayer != null) {
             mediaPlayer.release()
-            Log.i("LIFECYCLE","$mediaPlayer is successfully destroyed")
+            clicks.release()
+            Log.i("LIFECYCLE", "$mediaPlayer is successfully destroyed")
+            Log.i("LIFECYCLE", "$clicks is successfully destroyed")
+
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putInt("SCORE",_score)
-        Log.i("LIFECYCLE","Saved Score: $_score")
+        outState.putInt("SCORE", _score)
+        Log.i("LIFECYCLE", "Saved Score: $_score")
     }
 
 }
--- app/src/test/java/com/example/core1_ivansoohoo/MainActivityTest.kt
+++ app/src/test/java/com/example/core1_ivansoohoo/MainActivityTest.kt
@@ -1,18 +1,18 @@
-// package com.example.core1_ivansoohoo
-//
-// import android.content.pm.ActivityInfo
-// import androidx.test.espresso.Espresso.onView
-// import androidx.test.espresso.action.ViewActions.click
-// import androidx.test.espresso.assertion.ViewAssertions.matches
-// import androidx.test.espresso.matcher.ViewMatchers.*
-// import androidx.test.ext.junit.rules.ActivityScenarioRule
-// import androidx.test.ext.junit.runners.AndroidJUnit4
-// import androidx.test.filters.LargeTest
-// import org.junit.Before
-// import org.junit.Rule
-// import org.junit.Test
-// import org.junit.runner.RunWith
-//
+ package com.example.core1_ivansoohoo
+
+ import android.content.pm.ActivityInfo
+ import androidx.test.espresso.Espresso.onView
+ import androidx.test.espresso.action.ViewActions.click
+    import androidx.test.espresso.assertion.ViewAssertions.matches
+ import androidx.test.espresso.matcher.ViewMatchers.*
+ import androidx.test.ext.junit.rules.ActivityScenarioRule
+ import androidx.test.ext.junit.runners.AndroidJUnit4
+ import androidx.test.filters.LargeTest
+ import org.junit.Before
+ import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
 // @LargeTest
 // @RunWith(AndroidJUnit4::class)
 // class MainActivityTest {
